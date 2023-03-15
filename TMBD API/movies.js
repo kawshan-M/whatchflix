249a7695136b987bc03a7d3c049af557449bb85a
DESKTOP-H4K489P\vinil <vinilperera2@gmail.com>
2023-03-15 21:58:34 +0530

    Update to output the genre in the word format

--- TMBD API/movies.js
+++ TMBD API/movies.js
@@ -1,10 +1,12 @@
 const apiKey = '8af35d2b86bd21843cff860c18d30657';
-const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`;
+const trendingUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`;
+const genresUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`;
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const movies = data.results;
+Promise.all([fetch(trendingUrl), fetch(genresUrl)])
+    .then(([trendingResponse, genresResponse]) => Promise.all([trendingResponse.json(), genresResponse.json()]))
+    .then(([trendingData, genresData]) => {
+        const movies = trendingData.results;
+        const genres = genresData.genres;
         const moviesList = document.getElementById("movies-list");
 
         movies.forEach(movie => {
@@ -17,7 +19,7 @@ fetch(url)
             const description = document.createElement("p");
 
             title.innerText = movie.title;
-            genre.innerText = movie.genre_ids.join(", ");
+            genre.innerText = movie.genre_ids.map(genreId => genres.find(genre => genre.id === genreId).name).join(", ");
             thumbnail.src = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
             thumbnail.alt = movie.title + " thumbnail";
             imdbId.innerText = `IMDb ID: ${movie.id}`;
